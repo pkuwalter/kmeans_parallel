efec199c7451a3705cd82832c94eacb0cd5a4694
yige-hu <yige@cs.utexas.edu>
2015-03-05 16:57:55 -0600

    check in a cpu baseline

---
+++ baseline_cpu_seq/file_io.h
@@ -0,0 +1,38 @@
+#ifndef FILE_IO_H_
+#define FILE_IO_H_
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "kmeans.h"
+
+using namespace std;
+
+
+/*
+ * read input file.
+ * When allocating memory for input data, align in one-dimensional array to suit future changes to Cuda
+ * Performance issue: http://en.wikipedia.org/wiki/Data_structure_alignment
+ * Alternative: cudaMalloocPitch(), also has performance issue
+ * @param filename input file name
+ * @param num_points number of points
+ * @param num_coords number of coordinates
+*/
+float** file_read(char *filename, int  *num_points, int  *num_coords);
+
+/*
+ * write output file.
+ * @param filename input file name
+ * @param num_points number of points
+ * @param num_coords number of coordinates
+ * @param clusters [numClusters][numCoords] clusters centers
+ * @param membership [numObjs] membership of points to clusters
+*/
+int file_write(char *filename, int num_clusters, int num_points,
+		int num_coords, float **clusters, int *membership);
+
+
+#endif /* FILE_IO_H_ */
---
+++ baseline_cpu_seq/mtime.h
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <sys/time.h>
+#include <ctime>
+
+/*
+ *  * get elapsed time in milliseconds
+ *   */
+int64_t GetTimeMs64();
+
+/*
+ *  * get time duration in milliseconds
+ *   */
+int64_t GetTimeMsFrom(int64_t start);
+
+/*
+ *  * get elapsed time in milliseconds
+ *   */
+int64_t GetTimeMius64();
+
+/*
+ *  * get time duration in milliseconds
+ *   */
+int64_t GetTimeMiusFrom(int64_t start);
+
