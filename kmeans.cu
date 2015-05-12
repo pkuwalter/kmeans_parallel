a4b9c22bae2e210b3538459ff6f7298b31db43d2
yige-hu <yige@cs.utexas.edu>
2015-05-11 23:32:33 -0500

    typo

--- kmeans.cu
+++ kmeans.cu
@@ -451,9 +451,9 @@ printf("prep time = %lld microseconds\n", (long long) duration);
 start = GetTimeMius64();
 #endif
 
-    memset (device_pc_product, 0, pc_product_length);
-    memset (device_p_product, 0, p_product_length);
-    memset (device_c_product, 0, c_product_length);
+    memset (pc_product, 0, pc_product_length);
+    memset (p_product, 0, p_product_length);
+    memset (c_product, 0, c_product_length);
 
     // (x_i - c_j)^2 = (x_i)^2 + (c_j)^2 - 2*x_i*c_j
     // 1. Use cuBLAS to compute x_i*c_j
