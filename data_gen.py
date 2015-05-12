11765bcdab5b5acff3ae73737f2316913ee2373c
Zhiting Zhu <zzt0215@uw.edu>
2015-05-11 19:39:10 -0500

    modify data_gen.py to accept input arguments

--- data_gen.py
+++ data_gen.py
@@ -5,10 +5,7 @@ import random
 import cStringIO
 import struct
 
-def main():
-    num_points = 640
-    dimension = 5
-
+def gen_data(num_points,dimension,output_file):
     buffer = cStringIO.StringIO()
     for i in range(num_points):
         buffer.write(str(i))
@@ -18,12 +15,17 @@ def main():
             buffer.write(' ')
         buffer.write("\n")
 
-    with open("input.dat", "w") as f:
+    with open(output_file, "w+") as f:
         f.write(buffer.getvalue())
     buffer.close()
 
-                             
+def usage():
+    printf("Usage: %s [num_points] [dimention] [output_file]" % (sys.argv[0]))
+    sys.exit(1)
     
-
-if "__main__" == __name__:
-    main()
+if (len(sys.argv) != 4):
+    usage();
+num_points = int(sys.argv[1])
+dimension = int(sys.argv[2])
+output_file = sys.argv[3]
+gen_data(num_points, dimension, output_file)
